fe883db41e87fe31b1c77ebf1fc9ff5e4f91a383
bigbunjeeeeee <78023290+bigbunjeeeeee@users.noreply.github.com>
2022-02-07 10:19:28 +0000

    unity project

---
+++ Untitled Game/Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public float unitAmount;
+    public float Damage;
+    public float attackSpeed;
+    public float cost;
+    public float range;
+}
