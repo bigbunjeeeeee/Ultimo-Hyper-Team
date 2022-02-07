54ccf297bdf9bfdc6c84458f89292b2c2e9cf611
bigbunjeeeeee <78023290+bigbunjeeeeee@users.noreply.github.com>
2022-02-07 11:22:23 +0000

    game added

---
+++ NewGame/Assets/Scripts/EnemyValues.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyValues : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public float maxHealth;
+    public float damage;
+    public float aoe;
+    public float attackspeed;
+}
