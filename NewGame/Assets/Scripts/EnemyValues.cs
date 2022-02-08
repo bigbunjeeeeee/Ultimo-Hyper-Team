613ea316b2b91eb5e44500f8b9ab452cafca3e1a
bigbunjeeeeee <78023290+bigbunjeeeeee@users.noreply.github.com>
2022-02-08 10:35:35 +0000

    Basic AI
    Added Units attacking one another
    Added Bases attacking Units
    Added Units attacking Bases

--- NewGame/Assets/Scripts/EnemyValues.cs
+++ NewGame/Assets/Scripts/EnemyValues.cs
@@ -9,5 +9,39 @@ public class EnemyValues : MonoBehaviour
     public float maxHealth;
     public float damage;
     public float aoe;
+    float timer;
     public float attackspeed;
+
+    private void Start()
+    {
+        timer = attackspeed;
+    }
+    public void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(this);
+        }
+
+    }
+
+    private void Draw()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 2);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.tag != this.tag)
+            {
+                timerEnded(hitCollider);
+
+            }
+        }
+    }
+
+    void timerEnded(Collider2D target)
+    {
+        EnemyValues enemy = target.GetComponent<EnemyValues>();
+        enemy.health -= damage;
+
+    }
 }
