049b9f8138c6b3d420a42becd8bdf417634dc924
bigbunjeeeeee <78023290+bigbunjeeeeee@users.noreply.github.com>
2022-02-12 14:52:11 +0000

    Added Placing units
    added a button you press and can then place the units

---
+++ NewGame/Assets/Scripts/Player_Control.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    Vector2 movepoint;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = worldPos;
+
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
