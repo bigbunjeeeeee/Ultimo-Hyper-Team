104428653411261cc05f50b0c7cafcd613c732c5
bigbunjeeeeee <78023290+bigbunjeeeeee@users.noreply.github.com>
2022-03-04 13:40:31 +0000

    simple changes

--- NewGame/Assets/Scripts/testing.cs
+++ NewGame/Assets/Scripts/testing.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class testing : MonoBehaviour
 {
+    public int x;
+    public int y;
+    public float cells;
     // Start is called before the first frame update
     void Start()
     {
-        Grid grid = new Grid(20, 10, 10f);
+        Grid grid = new Grid(x, y, cells);
     }
 }
