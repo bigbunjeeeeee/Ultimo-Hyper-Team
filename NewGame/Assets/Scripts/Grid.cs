c7ce8fc75eeafa5a228bd688ce011009cf33596e
bigbunjeeeeee <78023290+bigbunjeeeeee@users.noreply.github.com>
2022-03-04 11:03:49 +0000

    grid

--- NewGame/Assets/Scripts/Grid.cs
+++ NewGame/Assets/Scripts/Grid.cs
@@ -2,38 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class Grid
 {
-    public Transform StartPos;
-    public LayerMask wallmask;
-    public Vector2 worldsize;
-    public float noderadius;
-    public float distance;
+    int width;
+    int height;
+    int[,] gridArray;
+    float cellsize;
 
-    Node[,] grid;
-    public List<Node> final_path;
-
-    float NodeDiamater;
-    int gridsizex, gridsizey;
-
-    private void Start()
+    public Grid(int width, int height, float cellsize)
     {
-        NodeDiamater = noderadius * 2;
-        gridsizex = Mathf.RoundToInt(worldsize.x / NodeDiamater);
-        gridsizey = Mathf.RoundToInt(worldsize.y / NodeDiamater);
+        this.width = width;
+        this.height = height;
+        this.cellsize = cellsize;
 
-        
-    }
-    void creategrid()
-    {
-        grid = new Node[gridsizex, gridsizey];
-        //Vector2 bottomleft = transform.position - Vector2.right * worldsize.x / 2 - Vector2.up * worldsize.y / 2;
-        for (int y = 0; y < gridsizey; y++)
+        gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
         {
-            for(int x = 0; x < gridsizex; x++)
+            for (int y = 0; y < gridArray.GetLength(1); y++)
             {
-                Vector2 worldPoint = 
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
         }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
+    Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellsize;
+    }
+
+    void GetXY(Vector3 worldPos, out int x, out int y)
+    {
+        x = Mathf.FloorToInt(worldPos.x / cellsize);
+        y = Mathf.FloorToInt(worldPos.y / cellsize);
+    }
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+        }
+
+    }
+    void SetValue(Vector3 worldpos, int value)
+    {
+        int x, y;
+        GetXY(worldpos, out x, out y);
+        SetValue(x, y, value);
+    }
+
 }
