9fb197a6cf4153ccae5345ba012254cab4460191
koreche0049 <78812184+koreche0049@users.noreply.github.com>
2022-03-14 14:35:05 +0000

    Removed Position.cs

--- NewGame/Assets/Scripts/BST.cs
+++ NewGame/Assets/Scripts/BST.cs
@@ -2,23 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BST <E>
+public class BST<E> where E : System.IComparable<E>
 {
-    
-        public struct Node// a node of the tree
+    public BST_NodeClass Root { get; set; }
+
+    public bool Add () 
+    {
+        BST_NodeClass<E> before = null, after = this.Root;
+        while (after!= null)
         {
-        Elem elt; // element value 
-        Node* par { get; set= null; }; //parent
-        Node* left; //left child
-        Node* right; //right child
-        Node() :  elt(), par(null),left(null),right(null) { };//constructor
+            before = after;
+            if(condition < after.Data)//Is new node in the left tree?
+            {
+                after = after.LeftNode;
+                
+            }
+            else if (condition > after.Data)//Is the new node in the right tree?
+            {
+                after = after.RightNode;
+            }
+            else
+            {
+                //Exist same value
+                return false;
+            }
+            BST_NodeClass<E> newNode = new BST_NodeClass<E>();
+            newNode.Data = condition;
+            if (this.Root == null)//Tree is empty
+                this.Root = newNode;
+            else
+            {
+                if (condition < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+            return true;
         }
-   
-        //public class Position;
-        
-    
-        //public int size() const ;
-        //public bool empty() const;
-        //public Position root() const;
+    }
         
 };
--- NewGame/Assets/Scripts/BST_NodeClass.cs
+++ NewGame/Assets/Scripts/BST_NodeClass.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class BST_NodeClass 
 {
-   
+   public BST_NodeClass InternalNode { get; set; }//Boolean node
+   public BST_NodeClass LeafNode { get; set; }//Do something node
+  
 };
